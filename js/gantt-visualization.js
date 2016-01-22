c90109ee413199d3ade450f2dd38345eda7764bd
Ade Crown <adecrown1@live.co.uk>
2016-01-22 14:42:28 +0000

    Fixed the the Edit/Add Item to chart problem

--- js/gantt-visualization.js
+++ js/gantt-visualization.js
@@ -1,20 +1,79 @@
- var items = new vis.DataSet([
-    {id: 'A', content: 'Period A', start: '2014-01-16', end: '2014-01-22', type: 'background'},
-    {id: 'B', content: 'Period B', start: '2014-01-25', end: '2014-01-30', type: 'background', className: 'negative'},
-    {id: 1, content: 'item 1<br>start', start: '2014-01-23'},
-    {id: 2, content: 'item 2', start: '2014-01-18'},
-    {id: 3, content: 'item 3', start: '2014-01-21'},
-    {id: 4, content: 'item 4', start: '2014-01-19', end: '2014-01-24'},
-    {id: 5, content: 'item 5', start: '2014-01-28', type:'point'},
-    {id: 6, content: 'item 6', start: '2014-01-26'}
-  ]);
 
-  var container = document.getElementById('gantt-visualization');
-  var options = {
-    start: '2014-01-10',
-    end: '2014-02-10',
+function __(id)
+{
+  return document.getElementById(id);
+}
+ var items = new vis.DataSet([]);
+
+ var container = document.getElementById('gantt-visualization');
+
+
+  var iName; var iStart; var iEnd;
+
+   var options =
+   {
+    onAdd: function(item, callback)
+    {
+      addNewGantt(item, callback)
+    },
+    onUpdate: function (item, callback)
+    {
+      updateGantt(item, callback)
+    },
+    //start: '2014-01-10',
+    //end: '2014-02-10',
     editable: true
   };
 
-  var timeline = new vis.Timeline(container, items, options);
 
+  function updateGantt(item, callback)
+  {
+    redirect("moreItems");
+    iName = __("itemName").value = item.content;
+    iStart = __("itemStart").value = item.start;
+    iEnd = __("itemEnd").value = item.end;
+    editGantt(item, callback);
+  }
+
+  function addNewGantt(item, callback)
+  {
+    redirect("moreItems");
+    editGantt(item, callback);
+  }
+
+function editGantt(item, callback)
+{
+  document.getElementById("done").onclick = function()
+  {
+    iName = __("itemName").value;
+    iStart = __("itemStart").value ;
+    iEnd = __("itemEnd").value;
+    item.content = iName;
+    item.start = iStart;
+    item.end = iEnd;
+    if (item.content != null) {
+      callback(item); // send back adjusted item
+      console.log(item);
+      emptyInput();
+      redirect("");
+    }
+    else {
+      callback(null); // cancel updating the item
+    }
+  }
+}
+
+
+  function redirect(id)
+  {
+    window.location = "#"+id;
+  }
+
+  function emptyInput()
+  {
+    __("itemName").value = "";
+    __("itemStart").value = "";
+    __("itemEnd").value = "";
+  }
+
+  var timeline = new vis.Timeline(container, items, options);
